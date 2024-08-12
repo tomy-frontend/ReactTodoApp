fffb7181273d472a84d675f85e938a520487e906
tomy-frontend <132113380+tomy-frontend@users.noreply.github.com>
2024-08-12 21:36:37 +0900

    Initial commit

---
+++ src/Todo.jsx
@@ -0,0 +1,10 @@
+import './index.css';
+
+export const Todo = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Thisis text</p>
+    </div>
+  );
+};
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Todo } from './Todo.jsx';
+import './index.css';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Todo />
+  </StrictMode>
+);
