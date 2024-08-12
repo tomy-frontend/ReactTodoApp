fccc5c71aa2b3169b5102f691b2ec501c69b50a3
tomy-frontend <xxx.d1231@gmail.com>
2024-08-12 22:15:59 +0900

    スタイルマークアップと、mapでlistのループ表示まで完了

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,10 +1,46 @@
+import { useState } from 'react';
 import './index.css';
 
 export const Todo = () => {
+
+  const [incompleteTodos, setIncompleteTodos] = useState(["ToDoです1", "ToDoです2"]);
+  const [completeTodos, setCompleteTodos] = useState(["完了したToDoです1", "完了したToDoです2"]);
+
   return (
-    <div>
-      <h1>Hello World</h1>
-      <p>Thisis text</p>
+    <>
+    <div className='input-area'>
+      <input type="text" placeholder='ToDoを入力' />
+      <button>追加</button>
     </div>
+    <div className='common-area incomplete-area'>
+      <p className='title'>未完了のToDo</p>
+      <ul>
+        {incompleteTodos.map((todo) => (
+            <li key= {todo}>
+              <div className='list-row'>
+              <p className='todo-item'>{todo}</p>
+              <button>完了</button>
+              <button>削除</button>
+              </div>
+            </li>
+          )
+        )}
+      </ul>
+    </div>
+    <div className='common-area complete-area'>
+      <p className='title'>完了したToDo</p>
+      <ul>
+      {completeTodos.map((todo) => (
+           <li key= {todo}>
+           <div className='list-row'>
+             <p className='todo-item'>{todo}</p>
+             <button>戻す</button>
+             </div>
+           </li>
+          )
+        )}
+      </ul>
+    </div>
+    </>
   );
 };
